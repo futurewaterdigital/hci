c3acdc73dfed0ffcc7d79bd40dfe7e006d4a4d99
jalal1985khan <jalal1985khan@gmail.com>
2024-09-15 01:58:26 +0530

    hello

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import './globals.css'
 
 export const metadata: Metadata = {
   title: 'Healthcare International',
-  description: 'Healthcare International',
+  description: 'Healthcare International.',
 }
 
 export default function RootLayout({
