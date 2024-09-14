889e6046035bfc7293fd0c5252c6e33f057e5ed5
jalal1985khan <jalal1985khan@gmail.com>
2024-09-15 02:33:51 +0530

    changes in title for testing purpose

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import Script from 'next/script'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'Healthcare International',
+  title: '1Healthcare International',
   description: 'Healthcare International.',
 }
 
