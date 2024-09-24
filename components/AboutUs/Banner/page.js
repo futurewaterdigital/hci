658c3009ae628970516b02ffa8e3be5f8a48396a
jalal1985khan <jalal1985khan@gmail.com>
2024-09-25 01:18:54 +0530

    created inside pages for treatment and cities

--- components/AboutUs/Banner/page.js
+++ components/AboutUs/Banner/page.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Banner() {
   return (
     <div className="relative h-[90vh]">
-      <div className="absolute inset-0 bg-cover bg-center bg-no-repeat bg-[url('/images/abouts-us.png')]"></div>
+      <div className="absolute inset-0 bg-cover bg-center bg-no-repeat bg-[url('/images/abouts-us-new.png')]"></div>
       <div className="absolute h-[170px] w-full bottom-0">
         <div className="absolute inset-0 bg-gradient-to-t from-[#0E56A0] via-[#0E56A0] to-transparent opacity-80"></div>
         <div className="relative flex flex-col items-center h-full text-white">
