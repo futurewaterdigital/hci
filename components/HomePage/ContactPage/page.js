a4dca16249b26efb4e0032611ba0102e1507321a
jalal1985khan <jalal1985khan@gmail.com>
2024-09-26 23:57:26 +0530

    updated text and image

--- components/HomePage/ContactPage/page.js
+++ components/HomePage/ContactPage/page.js
@@ -276,7 +276,101 @@ function ConsultantForm() {
                   </p>
                 )}
               </div>
+
               <div>
+                <div class="flex">
+                  {/* <label
+                    for="search-dropdown"
+                    class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
+                  >
+                    Your Email
+                  </label> */}
+                  <button
+                    id="dropdown-button"
+                    data-dropdown-toggle="dropdown"
+                    class="flex-shrink-0 z-10 inline-flex items-center py-2 px-4 text-sm font-medium text-center text-gray-900 bg-gray-100 border border-e-0 border-gray-300 dark:border-gray-700 dark:text-white rounded-s-lg hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-300 dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
+                    type="button"
+                  >
+                    Country{' '}
+                    <svg
+                      class="w-2.5 h-2.5 ms-2.5"
+                      aria-hidden="true"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 10 6"
+                    >
+                      <path
+                        stroke="currentColor"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="m1 1 4 4 4-4"
+                      />
+                    </svg>
+                  </button>
+                  <div
+                    id="dropdown"
+                    class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700"
+                  >
+                    <ul
+                      class="py-2 text-sm text-gray-700 dark:text-gray-200"
+                      aria-labelledby="dropdown-button"
+                    >
+                      <li>
+                        <a
+                          href="#"
+                          class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                        >
+                          Shopping
+                        </a>
+                      </li>
+                      <li>
+                        <a
+                          href="#"
+                          class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                        >
+                          Images
+                        </a>
+                      </li>
+                      <li>
+                        <a
+                          href="#"
+                          class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                        >
+                          News
+                        </a>
+                      </li>
+                      <li>
+                        <a
+                          href="#"
+                          class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                        >
+                          Finance
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                  <div class="relative w-full">
+                    <input
+                      type="tel"
+                      name="yourPhone"
+                      value={yourPhone}
+                      onChange={handleTextChange}
+                      maxLength="10"
+                      id="search-dropdown"
+                      class="block w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-e-lg rounded-s-gray-100 rounded-s-2 border border-gray-300 p-4"
+                      placeholder="Phone Number"
+                    />
+                  </div>
+                </div>
+                {errors.yourPhone && (
+                  <p className="text-red-500 text-start text-[14px]">
+                    {errors.yourPhone}
+                  </p>
+                )}
+              </div>
+
+              {/* <div>
                 <input
                   type="tel"
                   name="yourPhone"
@@ -291,7 +385,8 @@ function ConsultantForm() {
                     {errors.yourPhone}
                   </p>
                 )}
-              </div>
+              </div> */}
+
               <div>
                 <input
                   type="email"
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -23,7 +23,7 @@ const config: Config = {
       },
     },
   },
-  plugins: [flowbite, daisyui],
+  plugins: [flowbite, daisyui, require('flowbite/plugin')],
   daisyui: {
     themes: false, // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
     darkTheme: 'light', // name of one of the included themes for dark mode
