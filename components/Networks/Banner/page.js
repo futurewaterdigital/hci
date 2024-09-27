735f01540f4a98ce91e63d8f7c0777894298ba91
jalal1985khan <jalal1985khan@gmail.com>
2024-09-28 01:43:43 +0530

    updated banner

--- components/Networks/Banner/page.js
+++ components/Networks/Banner/page.js
@@ -10,7 +10,7 @@ function Banner({ banner, name }) {
         {/* {banner} */}
       </div>
       <div className="absolute h-[340px] w-full bottom-0">
-        <div className="absolute inset-0 bg-gradient-to-t from-[#0E56A0] via-[#0E56A0] to-transparent opacity-80"></div>
+        <div className="absolute inset-0 bg-gradient-to-t from-[#0e57a0c9] via-[#0E56A0] to-transparent opacity-80"></div>
         <div className="relative flex flex-col items-center text-white h-screen">
           <h1
             className="text-[150px] text-center font-medium"
--- components/Treatment/Banner/page.js
+++ components/Treatment/Banner/page.js
@@ -10,7 +10,7 @@ function Banner({ banner }) {
         {/* {banner} */}
       </div>
       <div className="absolute h-[150px] w-full bottom-0">
-        <div className="absolute inset-0 bg-gradient-to-t from-[#0E56A0] via-[#0E56A0] to-transparent opacity-80"></div>
+        <div className="absolute inset-0 bg-gradient-to-t from-[#0e57a0c9] via-[#0E56A0] to-transparent opacity-80"></div>
         {/* <div className="relative flex flex-col items-center h-full text-white">
           <h1 className="text-5xl text-center font-medium">
             You are in {city} city
