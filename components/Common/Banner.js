6704141b0f74987b9a4b50c4b5e28a3290459e69
jalal1985khan <jalal1985khan@gmail.com>
2025-02-08 10:25:16 +0530

    changed banner size

--- components/Common/Banner.js
+++ components/Common/Banner.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Banner({ image }) {
   return (
     <div
-      className="bg-center bg-no-repeat bg-contain h-[600px]"
+      className="bg-center bg-no-repeat bg-contain h-[700px]"
       style={{ backgroundImage: `url(${image})` }}
     >
       {/* {image} */}
