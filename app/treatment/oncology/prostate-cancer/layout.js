186ace18faa8e19a5596ff8be376965172751f41
jalal1985khan <jalal1985khan@gmail.com>
2025-04-26 02:17:15 +0530

    updated conflict mergred

--- app/treatment/oncology/prostate-cancer/layout.js
+++ app/treatment/oncology/prostate-cancer/layout.js
@@ -1,29 +1,29 @@
-import React from 'react'
-import Header from '@/components/Header/page'
-import Footer from '@/components/Footer/page'
+import React from "react";
+import Header from "@/components/Header/page";
+import Footer from "@/components/Footer/page";
 
 export const metadata = {
-  title: 'Cerebral Palsy',
-  description: 'Healthcare International.',
-  metadataBase: new URL('https://healthcareinternational.in/'),
+  title: "Prostate Cancer",
+  description: "Healthcare International.",
+  metadataBase: new URL("https://healthcareinternational.in/"),
   openGraph: {
-    url: 'https://healthcareinternational.in/',
-    title: 'Healthcare International',
-    description: 'Healthcare International',
+    url: "https://healthcareinternational.in/",
+    title: "Prostate Cancer",
+    description: "Prostate Cancer",
     images: [
       {
-        url: 'images/homeBanner.jpg',
+        url: "images/homeBanner.jpg",
         width: 800,
         height: 600,
-        alt: 'Healthcare International',
+        alt: "Healthcare International",
       },
     ],
   },
   icons: {
-    icon: 'favicon_io/favicon-32x32.png',
-    shortcut: 'favicon_io/favicon.ico',
+    icon: "favicon_io/favicon-32x32.png",
+    shortcut: "favicon_io/favicon.ico",
   },
-}
+};
 
 export default function RootLayout({ children }) {
   return (
@@ -32,5 +32,5 @@ export default function RootLayout({ children }) {
       {children}
       <Footer />
     </>
-  )
+  );
 }
