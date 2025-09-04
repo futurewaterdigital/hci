d54f59578eed664a0d32f160ffd827acdf47d7c0
sibiraj-002 <shansibi2002@gmail.com>
2025-09-04 22:28:57 +0530

    Schema updated in home page

--- app/page.tsx
+++ app/page.tsx
@@ -61,13 +61,66 @@ export default function Home() {
     ]
   };
 
+
+ const itemListSchema = {
+    "@context": "https://schema.org",
+    "@type": "ItemList",
+    "itemListElement": [
+      {
+        "@type": "ListItem",
+        "position": 1,
+        "item": {
+          "@type": "WebPage",
+          "url": "https://www.healthcareinternational.in/about-us",
+          "name": "About Us",
+          "description": "HCI has forged enduring alliances with some of India’s best known names in healthcare to offer patients a range of advanced treatments and procedures in various cities of their choice. We offer patients access to competent specialists, proven treatment protocols, and the best possible care, giving them the best possible chance of recovery.",
+          "publisher": {
+            "@type": "Organization",
+            "name": "Healthcare International",
+            "sameAs": "https://www.healthcareinternational.in"
+          }
+        }
+      },
+      {
+        "@type": "ListItem",
+        "position": 2,
+        "item": {
+          "@type": "WebPage",
+          "url": "https://www.healthcareinternational.in/our-services",
+          "name": "Healthcare International Services",
+          "description": "Healthcare International (HCI) is your trusted partner, supporting you at every stage of your medical journey.",
+          "publisher": {
+            "@type": "Organization",
+            "name": "Healthcare International",
+            "sameAs": "https://www.healthcareinternational.in"
+          }
+        }
+      },
+      {
+        "@type": "ListItem",
+        "position": 3,
+        "item": {
+          "@type": "WebPage",
+          "url": "https://www.healthcareinternational.in/contact-us",
+          "name": "HCI Contact Us",
+          "description": "Healthcare International is committed to building a strong, dependable healthcare universe that supports both patients and partners.",
+          "publisher": {
+            "@type": "Organization",
+            "name": "Healthcare International",
+            "sameAs": "https://www.healthcareinternational.in"
+          }
+        }
+      }
+    ]
+  };
+
   return (
     <main>
       {/* Pretty-printed JSON-LD for FAQs */}
       <script
         type="application/ld+json"
         dangerouslySetInnerHTML={{
-          __html: JSON.stringify(faqSchema, null, 2),
+          __html: JSON.stringify([itemListSchema, faqSchema], null, 2),
         }}
       />
       <Header />
